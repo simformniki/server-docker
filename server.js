0d64a919a681ee3449931a34d6bc618f3fb670ab
Niki <Niki.P@simformsolutions.com>
2019-07-04 18:32:43 +0530

    update mongo url

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const MongoClient = require("mongodb").MongoClient;
-const url = "mongodb://localhost:27017/";
+const url = "mongodb://172.16.1.154:27017/";
 var db;
 // Constants
 const PORT = 8080;
