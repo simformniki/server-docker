c6fb7e7c28fc6c12119fa68e15672a3a1f458fcb
Niki <Niki.P@simformsolutions.com>
2019-07-08 19:19:51 +0530

    yaml file for mongo and server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const MongoClient = require("mongodb").MongoClient;
-const url = "mongodb://172.16.1.154:27017/";
+const url = "mongodb://localhost:27017/";
 var db;
 // Constants
 const PORT = 8080;
@@ -10,12 +10,16 @@ const HOST = "0.0.0.0";
 
 //mongo connection
 MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
-  db = client.db("testdb");
-  console.log("connected to mongodb");
-  db.collection("test").save({ date: new Date() }, function(err, count) {
-    if (err) throw err;
-    console.log("Data: " + count);
-  });
+  if (err) {
+    console.log("Error while connecting to Mongo");
+  } else {
+    db = client.db("testdb");
+    console.log("connected to mongodb");
+    db.collection("test").save({ date: new Date() }, function(err, data) {
+      if (err) throw err;
+      else console.log("Data: " + data);
+    });
+  }
 });
 
 // App
